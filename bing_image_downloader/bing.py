ecaad133e0c6c6705890c6aee367045f2f025e8a
Philipp <phiyodr@gmail.com>
2022-03-17 13:15:46 +0100

    Save links in json and stop after too many empty pages

--- bing_image_downloader/bing.py
+++ bing_image_downloader/bing.py
@@ -4,6 +4,7 @@ import urllib
 import imghdr
 import posixpath
 import re
+import json
 
 '''
 Python api to download image form Bing.
@@ -12,7 +13,7 @@ Author: Guru Prasad (g.gaurav541@gmail.com)
 
 
 class Bing:
-    def __init__(self, query, limit, output_dir, adult, timeout,  filter='', verbose=True):
+    def __init__(self, query, limit, output_dir, adult, timeout,  filter='', max_empty_pages=500, verbose=True):
         self.download_count = 0
         self.query = query
         self.output_dir = output_dir
@@ -20,6 +21,8 @@ class Bing:
         self.filter = filter
         self.verbose = verbose
         self.seen = set()
+        self.link_dict = {}
+        self.max_empty_pages = max_empty_pages
 
         assert type(limit) == int, "limit must be integer"
         self.limit = limit
@@ -73,14 +76,19 @@ class Bing:
             if file_type.lower() not in ["jpe", "jpeg", "jfif", "exif", "tiff", "gif", "bmp", "png", "webp", "jpg"]:
                 file_type = "jpg"
                 
+            last_url = link.rsplit('/', 1)[-1]
+            file_name = f"IMG_{self.download_count:05d}_{last_url}"
+            file_path = self.output_dir.joinpath(file_name)
+    
             if self.verbose:
                 # Download the image
-                print("[%] Downloading Image #{} from {}".format(self.download_count, link))
+                print("Downloading #{} from {} to {}".format(self.download_count, link, file_name))
                 
-            self.save_image(link, self.output_dir.joinpath("Image_{}.{}".format(
-                str(self.download_count), file_type)))
-            if self.verbose:
-                print("[%] File Downloaded !\n")
+
+            self.link_dict[file_name]=link
+            self.save_image(link, file_path)
+            #if self.verbose:
+            #    print("[%] File Downloaded!")
 
         except Exception as e:
             self.download_count -= 1
@@ -88,28 +96,44 @@ class Bing:
 
     
     def run(self):
-        while self.download_count < self.limit:
+        empty_page_count = 0
+        while (self.download_count < self.limit) and (empty_page_count <= self.max_empty_pages):
             if self.verbose:
-                print('\n\n[!!]Indexing page: {}\n'.format(self.page_counter + 1))
+                print("===============================================")
+                print("empty_page_count", empty_page_count)
+                print('[!!]Indexing page: {}'.format(self.page_counter + 1))
             # Parse the page source and download pics
             request_url = 'https://www.bing.com/images/async?q=' + urllib.parse.quote_plus(self.query) \
                           + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
                           + '&adlt=' + self.adult + '&qft=' + ('' if self.filter is None else self.get_filter(self.filter))
-            request = urllib.request.Request(request_url, None, headers=self.headers)
-            response = urllib.request.urlopen(request)
-            html = response.read().decode('utf8')
-            if html ==  "":
-                print("[%] No more images are available")
-                break
-            links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
-            if self.verbose:
-                print("[%] Indexed {} Images on Page {}.".format(len(links), self.page_counter + 1))
-                print("\n===============================================\n")
+            try:
+                request = urllib.request.Request(request_url, None, headers=self.headers)
+                response = urllib.request.urlopen(request)
+                html = response.read().decode('utf8')
+                if html ==  "":
+                    print("[%] No more images are available.")
+                    break
+                links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
+                if self.verbose:
+                    print("[%] Indexed {} Images on Page {}.".format(len(links), self.page_counter + 1))
 
-            for link in links:
-                if self.download_count < self.limit and link not in self.seen:
-                    self.seen.add(link)
-                    self.download_image(link)
+                empty_page = True
+                for link in links:
+                    if self.download_count < self.limit and link not in self.seen:
+                        self.seen.add(link)
+                        self.download_image(link)
+                        empty_page = False
+                        empty_page_count = 0
+                empty_page_count += empty_page
+
+            except:
+                print("Err for", request_url)
 
             self.page_counter += 1
-        print("\n\n[%] Done. Downloaded {} images.".format(self.download_count))
+        print("[%] Done. Downloaded {} images.".format(self.download_count))
+
+        dict_file = f'{self.output_dir}.json'
+        print(self.link_dict)
+        with open(dict_file, 'w') as fp:
+            json.dump(self.link_dict, fp)
+        print(f"Dict_file saved in {dict_file}.")
